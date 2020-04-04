4efce3b1fadd1e5a1fd6c08307f65ac83ea201c5
Datorium <hello@datorium.lv>
2020-04-04 12:07:34 +0300

    version 12:07

--- MyApp.cs
+++ MyApp.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GitHubWorkings
 {
-    public partial class Form1 : Form
+    public partial class MyApp : Form
     {
-        public Form1()
+        public MyApp()
         {
             InitializeComponent();
         }
